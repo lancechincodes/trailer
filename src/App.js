9f5fe9bd2964091393f92f53927528c237bfcceb
lancechincodes <lanceechin@gmail.com>
2022-07-14 17:50:39 -0500

    Successful api request for poster and backdrop (refactor later)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import { useState } from 'react' 
 
 function App() {
   const searchOptions = {
@@ -6,20 +6,29 @@ function App() {
     api: 'https://api.themoviedb.org/3/'
   }
 
+  const [poster, setPoster] = useState('')
+  const [backdrop, setBackdrop] = useState('')
+
   function getData() {
-    const url = `${searchOptions.api}search/movie?api_key=${searchOptions.key}&query=Jack+Reacher`
+    const url = `${searchOptions.api}search/movie?api_key=${searchOptions.key}&query=harry+potter`
 
     fetch(url)
       .then(res => res.json())
       .then(res => {
         console.log(res)
+        setPoster(res.results[0].backdrop_path)
+        setBackdrop(res.results[0].poster_path)
       })
       .catch(err => console.log(err))
   }
 
+  const imagePath = 'https://image.tmdb.org/t/p/w500'
+
   return (
     <div className="App">
       <button onClick={getData}>Get Jack Reacher Data</button>
+      <img src={imagePath + poster} alt="test poster"/>
+      <img src={imagePath + backdrop} alt="test backdrop"/>
     </div>
   );
 }
