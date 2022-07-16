78c1bb78434c88d36885ecbcd460279466ab918f
lancechincodes <lanceechin@gmail.com>
2022-07-16 16:43:13 -0500

    Complete home animations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion';
 import Home from './components/Home'
 import Browse from './components/Browse'
 
 function App() {
+  // provides info on current route location
+  const location = useLocation() 
   // const searchOptions = {
   //   key: process.env.REACT_APP_TMDB_KEY,
   //   api: "https://api.themoviedb.org/3/"
@@ -10,10 +13,13 @@ function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/browse" element={<Browse/>}/>
-      </Routes>
+      {/* AnimatePresence is used for exit transitions */}
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.key}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/browse" element={<Browse/>}/>
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,75 @@
 import '../styles/Home.css'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 import trailerLogo from '../assets/trailer-logo.svg'
 
-
 const homeVariants = {
+    hidden: {
+        opacity: 1
+    },
+    leave: {
+        transition: {
+            duration: .7,
+        },
+        opacity: 0,
+    }
+}
+
+const brandVariants = {
     hidden: {
         x: '-100vw'
     },
     visible: {
         x: 0,
         transition: {
+            delay: .5,
             duration: 1,
             type: "spring",
             stiffness: 70
+        }
+    },
+    leave: {
+        x: '100vw',
+        transition: {
+            duration: .5,
         }
     }
 }
 
 function Home() {
+    const [showBrand, setShowBrand] = useState(true)
     const navigate = useNavigate()
+
+    setTimeout(()=> {
+        setShowBrand(false)
+    }, 2000)
+
     setTimeout(()=> {
         navigate("/browse")
-    }, 3000)
+    }, 2500)
 
     return (
-        <AnimatePresence>
-            <div className="home">
-                <motion.div 
-                    className="full-brand"
-                    variants={homeVariants}
-                    initial="hidden"
-                    animate="visible"
-                >
-                    <img className="trailer-logo" src={trailerLogo} alt="Trailer Logo"/>
-                    <h1 className="title">TRAILER</h1>
-                </motion.div>
-            </div>
-        </AnimatePresence>
+        <motion.div className="home"
+            variants={homeVariants}
+            initial="hidden"
+            exit="leave"
+        >
+            <AnimatePresence>
+                {showBrand && (
+                    <motion.div 
+                        className="full-brand"
+                        variants={brandVariants}
+                        initial="hidden"
+                        animate="visible"
+                        exit="leave"
+                    >
+                        <img className="trailer-logo" src={trailerLogo} alt="Trailer Logo"/>
+                        <h1 className="title">TRAILER</h1>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </motion.div>
     )
 }
 
