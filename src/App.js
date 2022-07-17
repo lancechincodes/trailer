ed7e7c1e4c3e09c4f7edeae179cd8779a7516523
lancechincodes <lanceechin@gmail.com>
2022-07-17 13:56:29 -0500

    Update color scheme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion';
+import { useState, useEffect } from 'react'
 import Home from './components/Home'
 import Browse from './components/Browse'
 
 function App() {
   // provides info on current route location
   const location = useLocation() 
-  // const searchOptions = {
-  //   key: process.env.REACT_APP_TMDB_KEY,
-  //   api: "https://api.themoviedb.org/3/"
-  // }
+  
+  // types: movie or show
+  const [typeSearch, setTypeSearch] = useState('movie')
+  const [trendingToday, setTrendingToday] = useState('')
+  const [trendingThisWeek, setTrendingThisWeek] = useState('')
+
+  useEffect(() => {
+    getTrendingTodayData()
+    getTrendingThisWeekData()
+  }, [])
+
+  const searchOptions = {
+    key: process.env.REACT_APP_TMDB_KEY,
+    api: "https://api.themoviedb.org/3/"
+  }
+
+  function getTrendingTodayData() {
+    const url = `${searchOptions.api}trending/${typeSearch}/day?api_key=${searchOptions.key}`
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setTrendingToday(res.results[0].backdrop_path)
+      })
+      .catch(err => console.log(err))
+  }
+
+  function getTrendingThisWeekData() {
+    const url = `${searchOptions.api}trending/${typeSearch}/week?api_key=${searchOptions.key}`
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        setTrendingThisWeek(res.results[1].backdrop_path)
+      })
+      .catch(err => console.log(err))
+  }
 
   return (
     <div className="App">
@@ -17,7 +50,13 @@ function App() {
       <AnimatePresence>
         <Routes location={location} key={location.key}>
           <Route path="/" element={<Home/>}/>
-          <Route path="/browse" element={<Browse/>}/>
+          <Route 
+            path="/browse" 
+            element={<Browse
+              trendingToday={trendingToday}
+              trendingThisWeek={trendingThisWeek}
+            />}
+          />
         </Routes>
       </AnimatePresence>
     </div>
--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,13 +4,14 @@ import hamburger from '../assets/hamburger.svg'
 import search from '../assets/search.svg'
 import closeSearch from '../assets/close-search.svg'
 
-function Browse() {
+function Browse({trendingToday, trendingThisWeek}) {
+    const imagePath = 'https://image.tmdb.org/t/p/w500'
 
     return(
         <div className="browse"> 
 
             {/* Logo and hamburger */}
-            <heading className="heading-browse">
+            <header className="heading-browse">
                 <div className="logo-browse">
                     <img className="logo-image-browse" src={logoImage} alt="Trailer Logo"/>
                     <h1 className="logo-title-browse">TRAILER</h1>
@@ -18,7 +19,7 @@ function Browse() {
                 <div className="hamburger">
                     <img className="hamburger-image" src={hamburger} alt="Navigation Button"/>
                 </div>
-            </heading>
+            </header>
 
             {/* Input box */}
             <div className="input-box">
@@ -38,21 +39,28 @@ function Browse() {
             </div>
 
             {/* Trending boxes */}
-            <p className="top-movies">Top movies</p>
+            <p className="browse-section-title">Top movies</p>
             <div className="trending-boxes">
                 <div className="trending-box">
-                    <h3 className="trending-text">Trending Today</h3>
+                    <h3 className="trending-text">Trending today</h3>
                     <div className="trending-poster-div">
-                        <img className="trending-poster" src={hamburger} alt="Top trending poster today"/>
+                        <img className="trending-poster" src={imagePath + trendingToday} alt="Top trending poster today"/>
                     </div>
                 </div>
                 <div className="trending-box">
                     <h3 className="trending-text">Trending this week</h3>
                     <div className="trending-poster-div">
-                        <img className="trending-poster" src={hamburger} alt="Top trending poster this week"/>
+                        <img className="trending-poster" src={imagePath + trendingThisWeek} alt="Top trending poster this week"/>
                     </div>
                 </div>
             </div>
+
+            {/* Genre boxes */}
+            <p className="browse-section-title">Browse all</p>
+            <div className="genre-container">
+
+            </div>
+
 
         </div>
     )
