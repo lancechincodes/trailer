bf9ff6ee2f203e4c572e5b150ef85382114fcbc3
lancechincodes <lanceechin@gmail.com>
2022-07-16 21:28:59 -0500

    Add top section (logo, hamburger menu, and search form) in Browse

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,23 +1,39 @@
 import '../styles/Browse.css'
 import logoImage from '../assets/logo-image.svg'
 import hamburger from '../assets/hamburger.svg'
+import search from '../assets/search.svg'
+import closeSearch from '../assets/close-search.svg'
 
 function Browse() {
 
     return(
         <div className="browse"> 
-            <div className="logo-browse">
-                <img className="logo-image-browse" src={logoImage} alt="Trailer Logo"/>
-                <h1 className="logo-title-browse">TRAILER</h1>
-            </div>
-            <div className="hamburger">
-                <img className="hamburger-image" src={hamburger} alt="Navigation Button"/>
-            </div>
-            <form type="submit">
-                <input type="text"></input>
-            </form>
+            <heading className="heading-browse">
+                <div className="logo-browse">
+                    <img className="logo-image-browse" src={logoImage} alt="Trailer Logo"/>
+                    <h1 className="logo-title-browse">TRAILER</h1>
+                </div>
+                <div className="hamburger">
+                    <img className="hamburger-image" src={hamburger} alt="Navigation Button"/>
+                </div>
+            </heading>
 
-        
+
+            <div className="input-box">
+                <div className="search-icon-div">
+                    <img className="search-icon" src={search} alt="Search Icon"/>
+                </div>
+                <form type="submit">
+                    <input 
+                        className="input-text"
+                        type="text"
+                        placeholder="Search movie, show, or genre"
+                    />
+                </form>
+                <div className="close-search-icon-div"> 
+                    <img className="close-search-icon" src={closeSearch} alt="Delete Icon"/>
+                </div>
+            </div>
         </div>
     )
 }
